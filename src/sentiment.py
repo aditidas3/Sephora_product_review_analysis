2d4b392b00d670165abd0a06ba17b4bb643beb2e
Aditi <addas@ucsd.edu>
2026-01-15 23:43:33 -0800

    Adding recommendation file

--- src/sentiment.py
+++ src/sentiment.py
@@ -3,9 +3,11 @@ import string
 import re
 import nltk
 from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords, wordnet
-from nltk.stem import WordNetLemmatizer, PorterStemmer
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 from collections import Counter
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+analyzer = SentimentIntensityAnalyzer()
 
 # importing necessary libraries
 nltk.download('punkt')
@@ -46,3 +48,27 @@ def wordCounter(df):
     all_words = [word for sentence in df['clean_text'] for word in word_tokenize(sentence.lower())]
     words_count = Counter(all_words)
     return words_count
+
+def sentiment_vader(text):
+    if pd.isna(title) or title == '':
+      return ''
+    title = re.sub(r'(.)\1{2,}', r'\1', title)
+    title = re.sub(r'[^a-zA-Z\s]', '', title)
+    if pd.isna(title) or title == '':
+        return ''
+    tokens = word_tokenize(title)
+    clean_tokens = []
+    for token in tokens:
+        token = token.lower()
+        if token not in stop_words and token.isalpha():
+            lemmatized = lemmatizer.lemmatize(token)
+            clean_tokens.append(lemmatized)
+    clean_text = ' '.join(clean_tokens)
+    sentiment_score = analyzer.polarity_scores(clean_text)['compound']
+    if sentiment_score >=0.05:
+        sentiment_label = 'positive'
+    elif sentiment_score<= -0.05:
+        sentiment_label = 'negative'
+    else:
+        sentiment_label = 'neutral'
+    return sentiment_score,sentiment_label
